9f6595d0625ad4e875ac5b95b5392a073b6aef8f
yemisrch <123356238+yemisrch@users.noreply.github.com>
2023-05-04 15:17:54 +0300

    Added a function to handle user turns and that checks who the winner is

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -51,7 +51,32 @@ def display_board():
 
 # Handle a turn for an arbitrary player
 def handle_turn(player):
-    pass
+    # Get position from player
+    print(player + "'s turn.")
+    position = input("Choose a position from 1-9: ")
+
+    # Whatever the user inputs, make sure it is a valid input, and the spot is open
+    valid = False
+    while not valid:
+
+        # Make sure the input is valid
+        while position not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            position = input("Choose a position from 1-9: ")
+
+        # Get correct index in our board list
+        position = int(position) - 1
+
+        # Then also make sure the spot is available on the board
+        if board[position] == "-":
+            valid = True
+        else:
+            print("You can't go there. Go again.")
+
+    # Put the game piece on the board
+    board[position] = player
+
+    # Show the game board
+    display_board()
 
 
 # Check if the game is over
@@ -62,7 +87,21 @@ def check_if_game_over():
 
 # Check to see if somebody has won
 def check_for_winner():
-   pass
+   # Set global variables
+    global winner
+    # Check if there was a winner anywhere
+    row_winner = check_rows()
+    column_winner = check_columns()
+    diagonal_winner = check_diagonals()
+    # Get the winner
+    if row_winner:
+        winner = row_winner
+    elif column_winner:
+        winner = column_winner
+    elif diagonal_winner:
+        winner = diagonal_winner
+    else:
+        winner = None
 
 
 # Check the rows for a win
