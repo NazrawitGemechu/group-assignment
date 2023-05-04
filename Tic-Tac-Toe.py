1d6b1a43ab54bfd8c8b41fbd59d1e84056f79621
s-a-m-r-i <127822806+s-a-m-r-i@users.noreply.github.com>
2023-05-04 15:25:10 +0300

    Add function to check the rows and the columns

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -106,11 +106,47 @@ def check_for_winner():
 
 # Check the rows for a win
 def check_rows():
-   pass
+   # Set global variables
+    global game_still_going
+    # Check if any of the rows have all the same value (and is not empty)
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+    # If any row does have a match, flag that there is a win
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    # Return the winner
+    if row_1:
+        return board[0]
+    elif row_2:
+        return board[3]
+    elif row_3:
+        return board[6]
+    # Or return None if there was no winner
+    else:
+        return None
 
 # Check the columns for a win
 def check_columns():
-    pass
+    # Set global variables
+    global game_still_going
+    # Check if any of the columns have all the same value (and is not empty)
+    column_1 = board[0] == board[3] == board[6] != "-"
+    column_2 = board[1] == board[4] == board[7] != "-"
+    column_3 = board[2] == board[5] == board[8] != "-"
+    # If any row does have a match, flag that there is a win
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+    # Return the winner
+    if column_1:
+        return board[0]
+    elif column_2:
+        return board[1]
+    elif column_3:
+        return board[2]
+    # Or return None if there was no winner
+    else:
+        return None
 
 
 # Check the diagonals for a win
